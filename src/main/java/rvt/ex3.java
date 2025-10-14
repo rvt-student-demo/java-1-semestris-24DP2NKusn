4899f6f77b5658b0cb9226f679705af3526c1118
Nikolajs Kušnarjovs <A240402nk@rvt.lv>
2025-10-14 10:31:07 +0300

    ex3

---
+++ src/main/java/rvt/ex3.java
@@ -0,0 +1,21 @@
+package rvt;
+import java.util.Scanner;
+public class ex3 {
+    public static void main(String[] args) {
+        
+       System.out.println("Ievadiet veselo skaitlu.");
+       Scanner scanner = new Scanner(System.in);
+
+      int n = scanner.nextInt();
+      int summ = 0;
+
+     while (n > 0) {
+        summ = summ + n*n;
+        n = n - 1;
+     }
+      System.out.println(summ);
+
+      scanner.close();
+    }
+}
+

77d1dc5bb027c5e9563316f6fce86777b33d156a
Nikolajs Kušnarjovs <A240402nk@rvt.lv>
2025-10-14 10:34:44 +0300

    From_parameter_to_one

---
+++ src/main/java/rvt/From_parameter_to_one.java
@@ -0,0 +1,16 @@
+package rvt;
+
+public class From_parameter_to_one {
+public static void main(String[] args){
+printFromNumber(2);
+}
+public static void printFromNumber(int number){
+
+for (int i = number; i > 0; i--) {
+System.out.println(i);
+
+}
+
+
+}
+}

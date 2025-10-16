652b38c3a36cf2f85f3cd95d2936ac3ecca8c322
[Nikolaj Kushnarjov] <[A240402NK@rvt.lv]>
2025-10-16 22:58:20 +0300

    JMArrayList

--- src/main/java/rvt/JMArrayList.java
+++ src/main/java/rvt/JMArrayList.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class JMArrayList {
     public static void main(String[] args) {
-        ListSize();
-
+        RemoveLast();
     }
+
+
     public static void onlyTheseNumbers() {
         Scanner input = new Scanner(System.in);
         ArrayList<Integer> numbers = new ArrayList<>();
@@ -21,11 +22,40 @@ public class JMArrayList {
                 numbers.add(ievads);
             }
         }
+
         System.out.println("From where?");
+        int start = Integer.valueOf(input.nextLine());
+        System.out.println("To where? ");
+        int stp = Integer.valueOf(input.nextLine());
+        for (int i = start; i <= stp; i++) {
+            System.out.println(numbers.get(i));
+        }
         
 
     }
+
+
     public static void ListSize() {
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        String ievads;
+        String stop = ".";
+
+        while (true) {
+            ievads = input.nextLine();
+            if (ievads.equals(stop)){
+                break;
+            } else{
+                list.add(ievads);
+            }
+        }
+        System.out.println("In total: " + list.size());
+
+    }
+
+
+
+    public static void OnTheList() {
         Scanner input = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
         String ievads;
@@ -39,10 +69,28 @@ public class JMArrayList {
                 list.add(ievads);
             }
         }
-        System.out.println("In total: " + list.size());
+        System.out.println("Search for?");
+        String search = input.nextLine();
+
+        if (list.contains(search)) {
+            System.out.println(search + " was found!");
+        } else {
+            System.out.println(search + " was not found!");
+        }
 
     }
-    public static void OnTheList() {}
-    public static void RemoveLast() {}
+
+    public static void RemoveLast() {
+
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+        if (!strings.isEmpty()) { 
+            strings.remove(strings.size() - 1); 
+        }
+
+        System.out.println(strings); 
+    }
 
 }

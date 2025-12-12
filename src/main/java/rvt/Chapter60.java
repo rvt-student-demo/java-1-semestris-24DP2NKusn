6b40b037598057f341eeca4d8e719f744d4fefdb
[Nikolaj Kushnarjov] <[A240402NK@rvt.lv]>
2025-12-12 19:34:11 +0200

    Chapter60: ex1

---
+++ src/main/java/rvt/Chapter60.java
@@ -0,0 +1,16 @@
+package rvt;
+
+public class Chapter60 {
+    public static void main(String args[]) {
+
+    }
+    public static void ex1() {
+        int[] val = {0, 1, 2, 3};
+        int sum = val[0] + val[1] + val[2] + val[3];
+        System.out.println( "Sum of all numbers = " + sum );
+    }
+    public static void ex2() {}
+    public static void ex3() {}
+    public static void ex4() {}
+    public static void ex5() {}
+}

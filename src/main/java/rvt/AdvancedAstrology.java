5bbdfb08da3ac3fa2e8e6351c8411b2c32b9f9e0
[Nikolaj Kushnarjov] <[A240402NK@rvt.lv]>
2025-12-12 19:48:26 +0200

    AdvancedAstrology: Pirma daļa

---
+++ src/main/java/rvt/AdvancedAstrology.java
@@ -0,0 +1,22 @@
+package rvt;
+
+import java.util.*;
+
+public class AdvancedAstrology {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printSpaces(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.print(" ");
+        }
+    }
+
+    public static void printStars(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
+    }
+}

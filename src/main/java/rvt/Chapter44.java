cff8d4e61b24e366b9b4898be68d0a9671a97a65
[Nikolaj Kushnarjov] <[A240402NK@rvt.lv]>
2025-12-12 19:03:05 +0200

    Chapter44: ex2

--- src/main/java/rvt/Chapter44.java
+++ src/main/java/rvt/Chapter44.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Chapter44 {
     public static void main(String args[]) {
-        
+
     }
     public static void ex1() {
         Scanner input = new Scanner(System.in);
@@ -23,11 +23,20 @@ public class Chapter44 {
         System.out.println(Name + Surname);
     }
 
-        public static void ex2() {}
-        public static void ex3() {}
-        public static void ex4() {}
-        public static void ex5() {}
-        public static void ex6() {}
-        public static void ex7() {}
+    public static void ex2() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+
+        String UserString = input.nextLine();
+
+        for (int i = 0; i < UserString.length(); i++) {
+            System.out.println(UserString.charAt(i));
+        }
+    }
+    public static void ex3() {}
+    public static void ex4() {}
+    public static void ex5() {}
+    public static void ex6() {}
+    public static void ex7() {}
 
 }

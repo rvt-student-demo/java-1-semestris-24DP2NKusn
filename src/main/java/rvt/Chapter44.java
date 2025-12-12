ad56904a03e2588d362f506a96a00b0fc501bd5e
[Nikolaj Kushnarjov] <[A240402NK@rvt.lv]>
2025-12-12 18:51:38 +0200

    Java-mooc 1-6: Grades and points

--- src/main/java/rvt/Chapter44.java
+++ src/main/java/rvt/Chapter44.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Chapter44 {
     public static void main(String args[]) {
-        ex3();
+        
     }
     public static void ex1() {
         Scanner input = new Scanner(System.in);
@@ -23,38 +23,9 @@ public class Chapter44 {
         System.out.println(Name + Surname);
     }
 
-        public static void ex2() {
-            Scanner input = new Scanner(System.in);
-            System.out.println("Enter a string: ");
-
-            String UserString = input.nextLine();
-
-            for (int i = 0; i < UserString.length(); i++) {
-                System.out.println(UserString.charAt(i));
-            }
-        }
-        public static void ex3() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String UserName = input.nextLine();
-
-        if (UserName.startsWith("Amy") ||  UserName.startsWith("Buffy") || UserName.startsWith("Cathy")){
-            System.out.println("Ms." + UserName);
-        } else if (UserName.startsWith("Elroy") ||  UserName.startsWith("Fred") || UserName.startsWith("Graham")) {
-            System.out.println("Mr." + UserName);
-        } else {
-            System.out.println(UserName);
-        }
-        }
-        public static void ex4() {
-            Scanner input = new Scanner(System.in);
-            System.out.println("Enter cook time: ");
-            String Time = input.nextLine();
-
-            if (2 <= Time.length()) {
-                
-            }
-        }
+        public static void ex2() {}
+        public static void ex3() {}
+        public static void ex4() {}
         public static void ex5() {}
         public static void ex6() {}
         public static void ex7() {}
--- src/main/java/rvt/Divisible_by_three.java
+++ src/main/java/rvt/Divisible_by_three.java
@@ -3,17 +3,17 @@ package rvt;
 public class Divisible_by_three {
 public static void main(String[] args){
 
-divisibleByThreeInRange(2, 10);
+    divisibleByThreeInRange(2, 10);
 }
+
 public static void divisibleByThreeInRange(int start, int end){
-for (int i = start; i <= end; i++) {
-if ( i % 3 == 0) {
-System.out.println(i);
-}
-}
+    for (int i = start; i <= end; i++) {
+        if ( i % 3 == 0) {
+            System.out.println(i);
+        }
+    }
 
 }
-
 
 }
 
